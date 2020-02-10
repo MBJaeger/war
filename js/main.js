3e157b897ee581519a9d5ce295fab99f1e64af26
MBJaeger <57914776+MBJaeger@users.noreply.github.com>
2020-02-10 10:09:52 -0600

    html and initial css

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d0
 "c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06",
 "s05","s04","s03","s02"];
 
-const Athlete1 = document.getElementsByClassName('athleteOne');
-const Athlete2 = document.getElementsByClassName('athleteTwo');
+const athlete1 = document.getElementsByClassName('athleteOne');
+const athlete2 = document.getElementsByClassName('athleteTwo');
+const warZone = document.getElementsByClassName('war-zone');
 const rBtn = document.getElementById('buttonReset');
 const pBtn = document.getElementById('buttonPlay');
 
 /*----- app's state (variables) -----*/
 
-
+ß
 
 /*----- cached element references -----*/
 
