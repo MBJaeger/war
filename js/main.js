ceb441c9fd594d55fa84c87c0df9b36f0c971f4b
MBJaeger <57914776+MBJaeger@users.noreply.github.com>
2020-02-12 11:00:22 -0600

    add files

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 /*----- constants -----*/
-//const cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ",
-// "hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09",
-// "c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06",
-// "s05","s04","s03","s02"];
+const cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ",
+"hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09",
+"c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06",
+"s05","s04","s03","s02"];
 
 
 /*----- app's state (variables) -----*/
-var cardUp = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
-var suits = ["diamons", "hearts", "spades", "clubs"];''
-
-var playButton = document.querySelector('btnPlay');
+let pile1down = [];
+let pile1up= [];
+let pile2down = [];
+let pile2up = [];
+// var cardUp = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+// var suits = ["diamons", "hearts", "spades", "clubs"];
+//let winHand;
+//var playButton = document.querySelector('btnPlay');
 
 /*----- cached element references -----*/
+const pile1down = document.getElementById('pile1down')
+const pile1up = document.getElementById('pile1up')
+const pile2down = document.getElementById('pile2down')
+const pile2up = document.getElementById('pile2up')
+const playbtn = document.getElementById('playbtn')
+//const playReset?? - I could use play to play and reset when game is over
 
 
 /*----- event listeners -----*/
@@ -24,18 +34,45 @@ playButton.addEventListener('click', onClick);
 // console.log('working');
 // }
 
-function cards() {
-    cards = [];
-    for (s in suits) {
-    var suits = suits [s][0]
+function shuffle() {
+    let idx = cards.length, temp, rndIdx;
+    while (0 !== idx) {
+    rndIdx = Math.floor(Math.random() * idx);
+    idx -= 1;
+    temp = cards[idx];
+    cards[idx] = cards[rndIdx];
+    cards[rndIdx] = temp;
+    }
+};
 
-
+function dealCards() {
+    //shuffle();
+    if (cards.length) {
+        for (let i = 0; i < 26; i++) {
+        pile1distro = pile1Up.splice(0, 1);
+        pile1cards.push(pile1distro[0]);
+     }
+        for (let i = 0; i < 26; i++) {
+        pile2distro = pile2Up.splice(0, 1);
+        pile2cards.push(pile2distro[0]);
     }
 
-function shuffle(){
 
-}
 
+// function playCards() {
+//     if (p1Cards.length > 0 && p2Cards.length > 0) {
+//         p1Flipped = p1Cards.splice(0, 1);
+//         p1flip.classList.replace('outline', p1Flipped);
+//     
+//     }
+//     if (p2Cards.length > 0 && p2Cards.length > 0) {
+//         p2Flipped = p2Cards.splice(0, 1);
+//         p2flip.classList.replace('outline', p2Flipped);
+//         
+//     }
+//     playButton.classList.add('hidden');
+//     compareCards();
+// };
 
 
 
@@ -96,5 +133,33 @@ function shuffle(){
 
 
 
-
+function lookUp(x) {
+    if (`${x}` === 'dA' || `${x}` === 'cA' || `${x}` === 'sA' || `${x}` === 'hA') {
+        return 14;
+    } else if (`${x}` === 'dK' || `${x}` === 'cK' || `${x}` === 'sK' || `${x}` === 'hK') {
+        return 13;
+    } else if (`${x}` === 'dQ' || `${x}` === 'cQ' || `${x}` === 'sQ' || `${x}` === 'hQ') {
+        return 12;
+    } else if (`${x}` === 'dJ' || `${x}` === 'cJ' || `${x}` === 'sJ' || `${x}` === 'hJ') {
+        return 11;
+    } else if (`${x}` === 'd10' || `${x}` === 'c10' || `${x}` === 's10' || `${x}` === 'h10') {
+        return 10;
+    } else if (`${x}` === 'd09' || `${x}` === 'c09' || `${x}` === 's09' || `${x}` === 'h09') {
+        return 9;
+    } else if (`${x}` === 'd08' || `${x}` === 'c08' || `${x}` === 's08' || `${x}` === 'h08') {
+        return 8;
+    } else if (`${x}` === 'd07' || `${x}` === 'c07' || `${x}` === 's07' || `${x}` === 'h07') {
+        return 7;
+    } else if (`${x}` === 'd06' || `${x}` === 'c06' || `${x}` === 's06' || `${x}` === 'h06') {
+        return 6;
+    } else if (`${x}` === 'd05' || `${x}` === 'c05' || `${x}` === 's05' || `${x}` === 'h05') {
+        return 5;
+    } else if (`${x}` === 'd04' || `${x}` === 'c04' || `${x}` === 's04' || `${x}` === 'h04') {
+        return 4;
+    } else if (`${x}` === 'd03' || `${x}` === 'c03' || `${x}` === 's03' || `${x}` === 'h03') {
+        return 3;
+    } else {
+        return 2;
+    }
+};
 
